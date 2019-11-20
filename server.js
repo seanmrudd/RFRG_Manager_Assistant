2ced3fc42e45a790a088c0b141ce389b324372b5
shrudder <seanmrudd@me.com>
2019-11-19 20:43:48 -0500

    initial commit, set up server, running concurrently

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const logger = require("morgan");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, function () {
+    console.log(`Server now listening on PORT ${PORT}!`);
+});
